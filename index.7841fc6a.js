cbaee074d0d8d07174850439551a869c6dd59793
vladislavzaver91 <102727549+vladislavzaver91@users.noreply.github.com>
2022-10-10 18:34:01 +0000

    Deploying to gh-pages from @ vladislavzaver91/goit-js-hw-10@81acb3d1f7d51ba38a57144d93fd011a69171aba 🚀

---
+++ index.7841fc6a.js
@@ -0,0 +1,2 @@
+({input:document.querySelector("#search-box"),list:document.querySelector(".country-list")}).input.addEventListener("input",(function(e){e.preventDefault();const t=e.currentTarget.value.trim().toLowerCase();console.log(t)}));
+//# sourceMappingURL=index.7841fc6a.js.map
---
+++ index.8633f626.js
@@ -0,0 +1,2 @@
+[document.querySelector("#search-box"),document.querySelector(".country-list")][0].addEventListener("input",(function(e){e.preventDefault();var t=e.currentTarget.value.trim().toLowerCase();console.log(t)}));
+//# sourceMappingURL=index.8633f626.js.map
